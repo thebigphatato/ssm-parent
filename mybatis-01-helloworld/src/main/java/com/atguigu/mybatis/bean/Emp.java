8ad0370254eaadb8a759d681148a3f9b0f7e0301
Thebigpotato <1225913685@qq.com>
2025-06-16 20:45:32 +0800

    Commit add

---
+++ mybatis-01-helloworld/src/main/java/com/atguigu/mybatis/bean/Emp.java
@@ -0,0 +1,21 @@
+package com.atguigu.mybatis.bean;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+public class Emp {
+
+    //开启驼峰命名自动映射封装,将Bean中的对象的驼峰名自动映射到数据库中对应的字段名
+    //在配置文件中加mybatis.configuration.map-underscore-to-camel-case=true
+
+
+    private Integer id;
+    private String empName; // emp_name；  驼峰命名规则
+    private Integer age;
+    private Double empSalary; // emp_salary
+
+
+}
---
+++ mybatis-01-helloworld/src/main/java/com/atguigu/mybatis/mapper/EmpParamMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.mybatis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * ClassName: EmpParamMapper
+ * Package: com.atguigu.mybatis.dao
+ * Description:
+ *
+ * @Author the big potato
+ * @Create 2025/6/16 19:02
+ * @Version 19
+ */
+@Mapper
+public interface EmpParamMapper {
+}
